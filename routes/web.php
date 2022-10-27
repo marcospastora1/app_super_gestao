4647ffa92b97029bd1cdd1a8142001be8d08e6bf
Marcos <marcospastora@gmail.com>
2022-10-27 10:02:26 -0300

    aula 33

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,13 @@ Route::get('/', [PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [ContatoController::class, 'contato']);
+
+Route::get(
+    '/contato/{nome}/{categoria_id}',
+    function (
+        string $nome = 'Desconhecido',
+        int $categoria_id = 1 //1 - Informação
+    ) {
+        echo "Estamos aqui $nome -  $categoria_id";
+    }
+)->where('categoria_id', '[0-9]')->where('nome', '[A-Za-z]+');
