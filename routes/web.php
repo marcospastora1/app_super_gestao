9d3f06253f602241006605e8ea751d7b5791c1c3
Marcos <marcospastora@gmail.com>
2022-10-31 10:30:19 -0300

    Aula 40

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\TesteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -37,15 +38,7 @@ Route::prefix('app')->group(function () {
     })->name('app.produto');
 });
 
-Route::get('/rota1', function () {
-    echo 'rota 1';
-})->name('site.rota1');
-
-Route::get('/rota2', function () {
-    return redirect()->route('site.rota1');
-})->name('site.rota2');
-
-//Route::redirect('/rota2', 'rota1');
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('site.rota1');
 
 Route::fallback(function () {
     echo 'A rota acessada não existe. <a href="' . route('site.index') . '">Clique aqui</a> para ir para a página inicial';
