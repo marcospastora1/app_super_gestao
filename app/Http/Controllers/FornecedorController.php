a1da7ca0bde1f19d750d8e9596a919e0a2b61598
Marcos <marcos.santos@codenapp.com>
2022-11-03 18:09:33 -0300

    aula 51 - @switch/case

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -12,12 +12,24 @@ class FornecedorController extends Controller
             0 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '55.912.633/0001-68'
+                'cnpj' => '55.912.633/0001-68',
+                'ddd' => '27', //Espirito Santo
+                'telefone' => '0000-0000'
             ],
             1 => [
                 'nome' => 'Fornecedor 2',
-                'status' => 'S'
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '73', //Bahia
+                'telefone' => '0000-0000'
             ],
+            2 => [
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '32', //Minas Gerais
+                'telefone' => '0000-0000'
+            ]
 
         ];
         return view('app.fornecedor.index', compact('fornecedores'));
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -7,9 +7,28 @@
 
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
+    Fornecedor: {{ $fornecedores[2]['nome'] }}
     <br />
-    Status: {{ $fornecedores[1]['status'] }}
+    Status: {{ $fornecedores[2]['status'] }}
     <br />
-    CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Dado não foi preenchido' }}
+    CNPJ: {{ $fornecedores[2]['cnpj'] ?? 'Dado não foi preenchido' }}
+    <br />
+    Telefone: {{ $fornecedores[2]['ddd'] ?? '' }} {{ $fornecedores[2]['telefone'] ?? '' }}
+    <br>
+    @switch($fornecedores[2]['ddd'])
+        @case('27')
+            Vitória - ES
+        @break
+
+        @case('73')
+            Alcobaça - BA
+        @break
+
+        @case('32')
+            Juiz de Fora - MG
+        @break
+
+        @default
+            Estado não identificado
+    @endswitch
 @endisset
