8d5de6984fd0d411048814c7514c0daba3b5838c
Marcos <marcos.santos@codenapp.com>
2022-11-03 18:19:44 -0300

    aula 53 - @while

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -7,7 +7,8 @@
 
 
 @isset($fornecedores)
-    @for ($i = 0; isset($fornecedores[$i]); $i++)
+    @php  $i = 0 @endphp
+    @while (isset($fornecedores[$i]))
         Fornecedor: {{ $fornecedores[2]['nome'] }}
         <br />
         Status: {{ $fornecedores[2]['status'] }}
@@ -16,5 +17,6 @@
         <br />
         Telefone: {{ $fornecedores[2]['ddd'] ?? '' }} {{ $fornecedores[2]['telefone'] ?? '' }}
         <hr>
-    @endfor
+        @php $i++ @endphp
+    @endwhile
 @endisset
