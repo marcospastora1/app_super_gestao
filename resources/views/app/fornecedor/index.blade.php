f3f5b06939171b9c6b1fa95b76a54bceec092557
Marcos <marcos.santos@codenapp.com>
2022-11-03 18:16:35 -0300

    aula 52 - @for

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -7,28 +7,14 @@
 
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[2]['nome'] }}
-    <br />
-    Status: {{ $fornecedores[2]['status'] }}
-    <br />
-    CNPJ: {{ $fornecedores[2]['cnpj'] ?? 'Dado não foi preenchido' }}
-    <br />
-    Telefone: {{ $fornecedores[2]['ddd'] ?? '' }} {{ $fornecedores[2]['telefone'] ?? '' }}
-    <br>
-    @switch($fornecedores[2]['ddd'])
-        @case('27')
-            Vitória - ES
-        @break
-
-        @case('73')
-            Alcobaça - BA
-        @break
-
-        @case('32')
-            Juiz de Fora - MG
-        @break
-
-        @default
-            Estado não identificado
-    @endswitch
+    @for ($i = 0; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{ $fornecedores[2]['nome'] }}
+        <br />
+        Status: {{ $fornecedores[2]['status'] }}
+        <br />
+        CNPJ: {{ $fornecedores[2]['cnpj'] ?? 'Dado não foi preenchido' }}
+        <br />
+        Telefone: {{ $fornecedores[2]['ddd'] ?? '' }} {{ $fornecedores[2]['telefone'] ?? '' }}
+        <hr>
+    @endfor
 @endisset
