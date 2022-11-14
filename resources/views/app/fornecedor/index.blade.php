310e7c2a84837d829c2cf56622436bec459ea3f8
Marcos <marcospastora@gmail.com>
2022-11-13 21:50:20 -0300

    Aula 57 - variavel loop

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -6,7 +6,9 @@
 {{-- @unless executa se o resultado for false --}}
 
 @isset($fornecedores)
+
     @forelse ($fornecedores as $fornecedor)
+        Iteração atual: {{ $loop->iteration }} <br>
         Fornecedor: {{ $fornecedor['nome'] }}
         <br />
         Status: {{ $fornecedor['status'] }}
@@ -14,6 +16,15 @@
         CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não foi preenchido' }}
         <br />
         Telefone: {{ $fornecedor['ddd'] ?? '' }} {{ $fornecedor['telefone'] ?? '' }}
+        <br>
+        @if ($loop->first)
+            Primeira iteração do loop
+        @endif
+        @if ($loop->last)
+            Última iteração do loop
+            <br>
+            Total de registros: {{ $loop->count }}
+        @endif
         <hr>
     @empty
         Não existem fornecedores cdastrados
